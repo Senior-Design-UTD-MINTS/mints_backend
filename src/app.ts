32ba2af81a72e019927a128c03d303693083a40d
Beepbop0 <hey.nikhil42@gmail.com>
2019-10-31 14:47:31 -0500

    use typescript import instead of js style

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import request from "request";
+import cors from "cors";
 
 const app: express.Application = express();
 const port: number = 3000;
@@ -7,8 +8,7 @@ const DRUID_SQL_URL: string = "http://localhost:8082/druid/v2/sql/";
 const LATEST_DATA_QUERY: Object = {
   query: "SELECT * FROM INITIAL_DATA ORDER BY __time DESC LIMIT 1"
 };
-const cors = require('cors');
-app.use(cors());
+
 function handleLatestData(req: express.Request, res: express.Response): void {
   request.post(DRUID_SQL_URL, (error: any, _response: request.Response, body: any) => {
     if (body) {
@@ -24,6 +24,8 @@ function handleLatestData(req: express.Request, res: express.Response): void {
   }).json(LATEST_DATA_QUERY);
 }
 
+// setting up server and routing
+app.use(cors());
 app.get("/latestData", handleLatestData);
 app.get("/", (req, res) => {
   res.send("hello world");
